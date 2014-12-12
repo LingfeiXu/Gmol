96f065af00b54ba10c05114e3a897176c8cceec8
LingfeiXu <lxq35@hotmail.com>
2014-12-12 04:09:54 -0600

    update

--- gmol_v2.0/gmol_linux_2.0/src/org/jmol/api/JmolSyncInterface.java
+++
@@ -1,9 +0,0 @@
-package org.jmol.api;
-
-public interface JmolSyncInterface {
-
-  public abstract void syncScript(String script);
-  public abstract void register(String id, JmolSyncInterface jsi);
-  public Object getProperty(String key);
-
-}
